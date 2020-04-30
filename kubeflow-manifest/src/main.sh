89feb6ef93e1b70263364c97066f64095b3c7ee1
Zachary Seguin <zachary.seguin@canada.ca>
2020-04-30 16:14:09 -0400

    fix(kubeflow-manifest): Apply istio patches on kubeflow install

--- kubeflow-manifest/src/main.sh
+++ kubeflow-manifest/src/main.sh
@@ -4,7 +4,7 @@ function main {
   ./params.sh
   kfctl apply -f kfctl_k8s_istio.v1.0.1.yaml
   kubectl -n kubeflow patch --type=json gateway kubeflow-gateway -p '[{"op":"replace","path":"/spec/selector/istio","value":"ingressgateway-kubeflow"}]'
-  kubectl apply -f patches/kubeflow.yml
+  kubectl apply -f patches/kubeflow.yml -f patches/istio
   kubectl annotate mutatingwebhookconfigurations.admissionregistration.k8s.io admission-webhook-mutating-webhook-configuration certmanager.k8s.io/inject-ca-from=kubeflow/admission-webhook-cert --overwrite
 }
 
