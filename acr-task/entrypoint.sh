f193cf5e4c88aaa84e1e965a8b3227f6c639d8e2
William Hearn <sylus1984@gmail.com>
2020-07-14 19:11:58 -0400

    feat(ci): Add ACR task

---
+++ acr-task/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+INPUT_DOCKERFILE=${INPUT_DOCKERFILE:-Dockerfile}
+INPUT_TAG=${INPUT_TAG:-${GITHUB_SHA::8}}
+IMAGE_PART=""
+
+if [ "$INPUT_IMAGE" != "" ]; then
+  IMAGE_PART="/${INPUT_IMAGE}"
+fi
+
+echo "Building Docker image ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} from ${GITHUB_REPOSITORY} on ${INPUT_BRANCH} and using context ${INPUT_FOLDER} ; and pushing it to ${INPUT_REGISTRY} Azure Container Registry"
+
+az login --service-principal -u ${INPUT_SERVICE_PRINCIPAL} -p "${INPUT_SERVICE_PRINCIPAL_PASSWORD}" --tenant ${INPUT_TENANT}
+
+if [ -z "${INPUT_REPOSITORY_CRDS}" ]; then
+  az acr build -r ${INPUT_REGISTRY} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} https://"${INPUT_REPOSITORY_CRDS}"@github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}
+else
+  az acr build -r ${INPUT_REGISTRY} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} https://github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}
+fi
