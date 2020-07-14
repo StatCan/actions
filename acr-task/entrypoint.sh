7cfd025c3a6420d8df0626ea83776ab68cb63325
William Hearn <sylus1984@gmail.com>
2020-07-14 19:22:40 -0400

    feat(ci): Add ACR task

--- acr-task/entrypoint.sh
+++ acr-task/entrypoint.sh
@@ -16,7 +16,9 @@ echo "Building Docker image ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} from $
 az login --service-principal -u ${INPUT_SERVICE_PRINCIPAL} -p "${INPUT_SERVICE_PRINCIPAL_PASSWORD}" --tenant ${INPUT_TENANT}
 
 if [ ! -z "${INPUT_REPOSITORY_CRDS}" ]; then
+  echo "Executing against private repository"
   az acr build -r ${INPUT_REGISTRY} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} https://"${INPUT_REPOSITORY_CRDS}"@github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}
 else
+  echo "Executing agaisnt public repository"
   az acr build -r ${INPUT_REGISTRY} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} https://github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}
 fi
